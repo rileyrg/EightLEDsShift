7df02d63787f256af7fbd5997a2be03cfcfc3ac1
Richard Riley <rileyrg@gmail.com>
2019-09-17 16:01:00 +0200

    interrupt settng reset flag

--- src/main.cpp
+++ src/main.cpp
@@ -3,36 +3,59 @@
 
 //https://www.arduino.cc/en/Tutorial/ShiftOut
 
-#define SerialDataInPIn 4 //attach to SER
+#define SerialDataInPin 4 //attach to SER
 #define StorageRegisterClockPin 5 
 #define  ShiftRegisterClockPin 6 // attach to SRCLK
+#define SerialResetPin 7 // low to reset
+#define ResetButtonPin 2 // button to reset
 
 byte leds = 0;
+bool fReset=false;
 
-void updateShiftRegister() 
+void setSR(){
+     digitalWrite(SerialResetPin,HIGH);
+     fReset=false;
+}
+void resetSR(){
+     digitalWrite(SerialResetPin,LOW);
+     fReset=true;;
+}
+
+void updateShiftRegister(byte v) 
 {
+
      digitalWrite(StorageRegisterClockPin, LOW);
-     shiftOut(SerialDataInPIn, ShiftRegisterClockPin, LSBFIRST, leds);
+     shiftOut(SerialDataInPin, ShiftRegisterClockPin, LSBFIRST, v);
      digitalWrite(StorageRegisterClockPin, HIGH);
 }
 
 void setup() //called once
 {
      pinMode(StorageRegisterClockPin, OUTPUT);
-     pinMode(SerialDataInPIn, OUTPUT);  
+     pinMode(SerialDataInPin, OUTPUT);  
      pinMode(ShiftRegisterClockPin, OUTPUT);
+     pinMode(SerialResetPin, OUTPUT);
+     pinMode(ResetButtonPin, INPUT_PULLUP);
+
+     attachInterrupt(digitalPinToInterrupt(ResetButtonPin), resetSR,FALLING);
+     // attachInterrupt(digitalPinToInterrupt(ResetKeyPin), setSR,RISING);
+     setSR();
+     updateShiftRegister(leds);
+     
+
 }
 
 void loop() 
 {
-     leds = 0;
-     updateShiftRegister();
-     delay(500);
-     for (int i = 0; i < 256; i++){
-//          bitSet(leds, i);
-          leds=i;
-          updateShiftRegister();
-          delay(50);
+     if(fReset){
+          setSR();
+          updateShiftRegister(leds=0);
+          delay(2000);
+     }
+     else
+     {
+          updateShiftRegister(leds++);
+          delay(100);
      }
 }
  
